f60ad142bbc1b5e7f5e034e743fd228bac7aeaef
ioanadobrica <ioanadbc99@yahoo.com>
2021-05-20 08:20:45 +0300

    Seminar 11

---
+++ Seminar11/src/teste/StudentTestsTest.java
@@ -0,0 +1,45 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import clase.Student;
+
+public class StudentTestsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConstructorRightName() {
+		String nume= "Ion";
+		Student student=new Student(nume);
+	    assertEquals(nume, student.getNume());
+	}
+	
+	@Test
+	public void testListAddNote() {
+		Student student=new Student();
+		int nota=8;
+		student.adaugaNota(nota);
+		assertEquals(nota, student.getNota(0));
+	}
+	
+	@Test
+	public void testListSize() {
+		Student student=new Student();
+		int nota=8;
+		student.adaugaNota(nota);
+		assertEquals(1, student.getNote().size());
+	}
+	
+
+}
